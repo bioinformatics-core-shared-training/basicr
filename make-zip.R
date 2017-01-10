35f7f27dd97a8fb8b3eb3261f292b81b13f1db22
markdunning <mark.dunning@gmail.com>
2017-01-10 14:52:33 +0000

    add new script to create patients data

--- make-zip.R
+++ make-zip.R
@@ -1,6 +1,6 @@
 notebooks <- dir(pattern="*.Rmd")
 notebooks <- notebooks[-grep("solution",notebooks)]
-data <- c("ozone.csv","cancer.patients.txt","countData.txt","gene.description.txt","gene.expression.txt")
+data <- c("ozone.csv","cancer.patients.txt","countData.txt","gene.description.txt","gene.expression.txt","create-patients-data.R")
 
 file.remove("Basic_R_Course.zip")
 zip("Basic_R_Course.zip", files=c(data,notebooks))
